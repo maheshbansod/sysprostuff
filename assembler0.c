786c0d265dd9433d22a52880dc32d37119946495
maheshbansod <coolkevin011@gmail.com>
2018-09-24 11:00:33 +0530

    moved size constants, mnemtab, errmsgs to tables.h

--- assembler0.c
+++ assembler0.c
@@ -4,19 +4,6 @@
 
 #include "tables.h"
 
-#define SYMN 100
-#define MNEMN 17
-#define ERRN 100
-
-char mnem[][10] = {"STOP", "ADD", "SUB", "MULT",
-	"MOVER", "MOVEM", "COMP", "BC",
-	"DIV", "READ", "PRINT", "DS",
-	"DC", "START", "END", "LTORG",
-	"ORIGIN"};
-char errmsg[][100] = {"Invalid operand. Expected integer.",
-					"Invalid operand. Expected quoted integer.",
-					"Not an instruction"};
-
 int errn=0, symn = 0;
 
 struct symtab sym[SYMN];
--- tables.h
+++ tables.h
@@ -1,4 +1,8 @@
 
+#define SYMN 100
+#define MNEMN 17
+#define ERRN 100
+
 struct symtab {
 	char symbol[100];
 	char defined;
@@ -11,3 +15,14 @@ struct errtab {
 	int id;
 	int lno;
 };
+
+char mnem[][10] = {"STOP", "ADD", "SUB", "MULT",
+	"MOVER", "MOVEM", "COMP", "BC",
+	"DIV", "READ", "PRINT", "DS",
+	"DC", "START", "END", "LTORG",
+	"ORIGIN"};
+char errmsg[][100] = {"Invalid operand. Expected integer.",
+					"Invalid operand. Expected quoted integer.",
+					"Not an instruction"};
+
+
