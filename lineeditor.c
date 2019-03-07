4f26552437bef2f72b73ff4c320291105457a97f
maheshbansod <coolkevin011@gmail.com>
2019-03-07 12:32:28 +0530

    Changed line editor. It's complete now

--- lineeditor.c
+++ lineeditor.c
@@ -2,113 +2,319 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct line {
-	int lno;
-	char *data;
-	struct line* next;
-} Line;
+#define LSIZE 500
 
-int main(int argc, char **argv) {
-	char str[1000],exit=0;
-	int i=0, n= 0;
-	FILE *fp;
-	Line* head = (Line*)malloc(sizeof(Line));
-	Line *temp = head;
-	Line *temp2;
-	head->next = NULL;
+typedef struct editbuff {
+	char *line; //character string
+	struct editbuff *next;
+} List;
+List *head, *last;
 
+void readBuffer(FILE *f);
+int countLines();
+void eprint(int, int);
+void einsert(int);
+void edelete(int, int);
+void ecopy(int, int, int);
+void emove(int, int,int );
+void efind(int, int, char*);
+int countSpaces(char*);
+List* copyList(int, int);
+void pasteList(int, List*);
+
+main(int argc, char **argv) {
+	char cmdline[30],str[500],cmd, *filename;
+	char alive = 1, ch;
+	int n1, n2, n3, n;
+	FILE *file, *f2;
+	List *t;
+	
 	if(argc < 2) {
-		printf("Enter filename as argument.\n");
-		return -1;
-	}
-	if((fp = fopen(argv[1], "r"))==NULL) {
-		printf("Couldn't open the file.\n");
+		printf("No filename specified.\n");
 		return -1;
 	}
 
-	printf("Loading file...\n");
+	filename = argv[1];
+	file = fopen(filename, "r");
 
-	while(!feof(fp)) {
-		fgets(str, 999, fp);
-		temp->next = (Line*)malloc(sizeof(Line));
-		temp = temp->next;
-		temp->data = (char*)malloc(strlen(str)+1);
-		strcpy(temp->data, str);
-		temp->next = NULL;
-	}
-	fclose(fp);
-	fp = NULL;
-	printf("Done.\n");
+	readBuffer(file);
+	
+	printf("%d lines\n", countLines());
+	
+	while(alive) {
+		printf("? ");
+		fgets(cmdline, 30, stdin);
 
-	do {
-		printf("$ ");
-		fgets(str, 999, stdin);
-		switch(str[0]) {
-		case 'a':
-			if(str[1]!='\n') {
-				printf("Invalid command or wrong syntax for command 'a'\n");
-				break;
-			}
-			printf("Enter the data to append(enter -1 on a new line to stop):\n");
-			str[0]='\0';
-			temp = head;
-			while(temp->next != NULL) temp=temp->next;
+		n1 = n2 = 0;
 
-			while(1) {
-				fgets(str, 999, stdin);
-				if(atoi(str) == -1 && strlen(str)==3)
-					break;
-				temp->next = (Line*)malloc(sizeof(Line));
-				temp = temp->next;
-				temp->data = (char*)malloc(strlen(str)+1);
-				strcpy(temp->data, str);
-				temp->next = NULL;
-			}
+		sscanf(cmdline, "%c %d %d %d", &cmd, &n1, &n2, &n3);
+		n = countSpaces(cmdline);
 
+		switch(cmd) {
+		case 'e':
+			//Exit
+			printf("Exit\n");
+			alive = 0;
 			break;
 		case 'p':
-			if(str[1] != '\n') {
-				printf("Invalid command or wrong syntax for 'p'\n");
-				break;
+			//print
+			if(n < 2) {
+				n2 = 1;
+				if(n < 1)
+					n1 = 1;
 			}
-			temp = head->next;
-			i=1;
-			while(temp != NULL) {
-				printf("%d: %s",i++, temp->data);
-				temp=temp->next;
-			}
+			eprint(n1, n2);
 			break;
+		case 'i':
+			if(n < 1) n1 = 1;
+			einsert(n1);
+			break;
+		case 's':
+			if((fopen(filename,"r"))!=NULL) { //file exist
+				printf("Overwrite existing file(y/n)? ");
+				fgets(str, 500, stdin);
+				if(strcmp("n",str)==0) {
+					printf("Enter new filename: ");
+					fgets(filename,20 , stdin);
+				}
+			}
+			f2 = fopen(filename,"w");
 
-		case 'd':
-			i=sscanf(str,"%c %d", &i, &n);
-			printf("'%s', n'%d'\n",str,n);
-			if(i!=2) {
-				printf("Invalid command or invalid syntax for 'd'\n");
-				break;
+			t = head->next;	
+			while(t != NULL) {
+				fputs(t->line, f2);
+				fputc('\n',f2);
+				t = t->next;
 			}
-			temp = head;
-			i=1;
-			while(temp->next != NULL && i < n) {
-				temp = temp->next;
-				i++;
-			}
-			if(temp->next == NULL) {
-				printf("Line number out of  range\n");
-			} else if(i==n) {
-				temp2 = temp->next;
-				temp->next = temp2->next;
-				free(temp2->data);
-				temp2->data = NULL;
-				free(temp2);
-			}
+			fclose(f2);
 			break;
-		case 'e':
-			printf("Exit\n");
-			exit = 1;
+		case 'd':
+			if(n < 2) {
+				n2 = 1;
+				if(n < 1)
+					n1 = 1;
+			}
+			edelete(n1,n2);
+			break;
+		case 'c':
+			if(n<3) {
+				n3=1;
+				if(n<2) {
+					n2 = 1;
+					if(n<1)
+						n1=1;
+				}
+			}
+			ecopy(n1, n2, n3);
+			break;
+		case 'm':
+			if(n<3) {
+				n3=1;
+				if(n<2) {
+					n2 = 1;
+					if(n<1)
+						n1=1;
+				}
+			}
+			emove(n1,n2,n3);
+			break;
+		case 'f':
+			if(n<2) {
+				n2 = 1;
+				if(n<1)
+					n1=1;
+			}
+			printf("Enter pattern: ");
+			fgets(str, 500, stdin);
+			efind(n1, n2, str);
 			break;
 		default:
-			printf("Invalid command.\n");
+			printf("invalid operation.\n");
 			break;
 		}
-	}while(!exit);
+	}
+}
+
+void efind(int n1, int n2, char *str) {
+	List *t=head->next;
+	int n=1,c=0;
+
+	//navigate to line n1
+	while(t != NULL) {
+		t=t->next;
+		if(n==n1) break;
+		n++;
+	}
+
+	printf("Result:\n");
+
+	while( t!= NULL) {
+		if(strstr(t->line,str)) {
+			printf("%d: %s\n",n+1,t->line);
+			c++;
+		}
+		n++;
+		if(n>=n2) break;
+		t=t->next;
+	}
+	
+	if(c == 0) printf("No line contains the given pattern");
+}
+
+void emove(int n1, int n2, int n3) {
+	List *t = copyList(n1,n2);
+	edelete(n1,n2);
+	pasteList(n3,t);
+}
+
+void edelete(int n1, int n2) {
+	int n=1;
+	List *t = head->next, *p=head, *todel;
+	while(t != NULL) {
+		if(n1 <= n) {
+			todel = t;
+			p->next = t->next;
+			free(t->line);
+			free(todel);
+			t = p;
+			if(n2 <= n) return;
+		}
+		n++;
+		p = t;
+		t = t->next;
+	}
+}
+
+List* copyList(int n1, int n2) {
+	List *tn = (List*)malloc(sizeof(List)), *t = head, *tnh;
+	int n = 0;
+	tn->next = NULL;
+	tnh = tn;
+
+	while(n< n1 && t->next != NULL) {
+		t=t->next;
+		n++;
+	}
+
+	while( t != NULL) {
+		tn->next = (List*) malloc(sizeof(List));
+		tn=tn->next;
+		tn->line = (char*)malloc(sizeof(char)*(strlen(t->line)+1));
+		strcpy(tn->line,t->line);
+		tn->next = NULL;
+		if(n2<=n) break;
+		n++;
+		t=t->next;
+	}
+
+	return tnh;
+}
+
+void pasteList(int n1, List *l) {
+	int n=0;
+	List *t = head, *ln;
+	//navigate to line (n1-1)
+	while(t != NULL) {
+		n++;
+		if(n==n1) break;
+		t=t->next;
+	}
+	
+	//insert list
+	l = l->next;
+	while(l != NULL) {
+		ln = l->next;
+		l->next = t->next;
+		t->next = l;
+		l=ln;
+		t = t->next;
+	}	
+}
+
+void ecopy(int n1, int n2, int n3) {
+	List *t;
+	t = copyList(n1,n2);
+	pasteList(n3,t);
+}
+
+void einsert(int n1) {
+	int n=1;
+	char str[500];
+	List *t = head, *newnode;
+
+	//navigate to line n1
+	while(n < n1 && t->next != NULL) {
+		t = t->next;
+		n++;
+	}
+
+	printf("Enter text:\n");
+	while(1) {
+		fgets(str, 500, stdin);
+		if(strcmp(str, ".")==0) return;
+		newnode = (List*)malloc(sizeof(List));
+		newnode->next = t->next;
+		newnode->line = (char*)malloc(sizeof(char)*(strlen(str)+1));
+		strcpy(newnode->line, str);
+		t->next = newnode;
+		t = newnode;
+	}
+}
+
+void eprint(int n1, int n2) {
+	int n = 1;
+	List *t = head->next;
+	while(t != NULL) {
+		if(n1 <= n) {
+			printf("%d: %s\n",n, t->line);
+			if(n2 <= n) return;
+		}
+		t = t->next;
+		n++;
+	}
+}
+
+int countSpaces(char *str) {
+	int i=0, n=0;
+	while(str[i] != '\0') {
+		if(str[i]==' ') n++;
+		i++;
+	}
+	return n;
+}
+
+void readBuffer(FILE *f) {
+	char ch, linel[LSIZE], i=0;
+	List *t = NULL, *next;
+	head = (List*) malloc(sizeof(List));
+	head->next = NULL;
+	next = head;
+	if(f == NULL) return;
+	while((ch = fgetc(f))!=EOF) {
+		if(ch == '\n') {
+			//store to ll
+			linel[i]='\0';
+			t = (List *) malloc(sizeof(List));
+			next->next = t;
+			t->next = NULL;
+			t->line = (char *)malloc((strlen(linel)+1)*sizeof(char));
+			strcpy(t->line, linel);
+			next = next->next;
+			i=0;
+		} else {
+			linel[i++]=ch;
+		}
+	}
+
+	if(t!=NULL) last = t;
+}
+
+int countLines() {
+	int n=0;
+	List *t = head;
+	while(t->next != NULL) {
+		n++;
+		t = t->next;
+	}
+	return n;
 }
